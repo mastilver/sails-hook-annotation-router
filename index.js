7938bb2ed26ab692a891182eacd52b114fc56f07
Sileghem Thomas <tsileghem@neworbit.co.uk>
2015-05-28 13:13:19 +0100

    make it work on windows

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ module.exports = function(sails){
     return {
         initialize: function(callback){
 
-            var controllersFolder = sails.config.paths.controllers
+            var controllersFolder = sails.config.paths.controllers;
             var pattern = controllersFolder + '/**/*Controller.js';
 
             annotationRouter(pattern, function(err, route){
 
-                var controllerName = path.relative(controllersFolder, route.controller.full)
-                                         .slice(0, -route.controller.ext.length);
+                var controllerName = path.join(path.relative(controllersFolder, route.controller.dir), route.controller.name);
+
+                // make it work for windows
+                controllerName = controllerName.split(path.sep).join('/');
 
                 sails.config.routes[route.method + ' ' + route.url] = {
                     controller: controllerName,
